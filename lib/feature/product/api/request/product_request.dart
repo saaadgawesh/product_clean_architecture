0abde3309cbcd1155d631aa633d051fda915c51e
saaadgawesh <saadgawesh608@gmail.com>
2026-03-18 01:20:39 +0200

    product_clean_arch

--- lib/feature/product/api/request/product_request.dart
+++
@@ -1,9 +0,0 @@
-class ProductRequest {
-  //   @Query("page") int page,
-  // @Query("limit") int limit,
-  final int page;
-  final int limit;
-  ProductRequest({required this.page, required this.limit});
-
-  Map<String, dynamic> toJson() => {"page": page, "limit": limit};
-}
